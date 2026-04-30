b3d1623c08045af9556b09dd7ec58112d2573002
faikaanjum222 <faika.08.anjum@gmail.com>
2026-04-30 11:49:57 +0600

    Accessories
    Change Response to NextResponse only

--- stella/app/api/accessories/route.js
+++ stella/app/api/accessories/route.js
@@ -1,26 +1,21 @@
+import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
-
 export async function GET(request) {
     try {
         const url = new URL(request.url);
         const animalfilter = url.searchParams.get("animal");
-
         let querywhere = {
             category: "ACCESSORY"
         };
-
         if (animalfilter !== null && animalfilter !== "ALL") {
             querywhere.animalType = animalfilter;
         }
-
         const products = await prisma.product.findMany({
             where: querywhere
         });
-
-        return Response.json(products);
+        return NextResponse.json(products);
     } catch (error) {
-        return Response.json([]);
+        return NextResponse.json([]);
     }
 }
